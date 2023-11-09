75a779b4b2a8da6cdefba18e42b164d3762c4475
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-09 21:52:51 +0600

    This is my Country duide app

---
+++ script.js
@@ -0,0 +1,7 @@
+let searchBtn = document.getElementById("search-btn");
+
+let counteyInp = document.getElementById("country-inp");
+searchBtn.addEventListener("click", () => {
+  let countryName = counteyInp.Value;
+  let finalURL = ``;
+});

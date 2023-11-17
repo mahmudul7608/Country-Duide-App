13b8d177b224c5f7dd1a48951b31412102520be9
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-17 11:46:15 +0600

    change js section

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
 let searchBtn = document.getElementById("search-btn");
 
 let countryInp = document.getElementById("country-inp");
+
 searchBtn.addEventListener("click", () => {
-  let countryName = "India";
+  let countryName = countryInp.value;
   let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
   console.log(finalURL);
-  fetch(finalURL)
-    .then((response) => response.json())
-    // Change "then" to "data" in the second .then() block
-    .then((data) => {
-      console.log(data[0]);
-      console.log(data[0].capital[0]);
-      console.log(data[0].flags.svg);
-      console.log(data[0].name.common);
-      console.log(data[0].continents[0]); // Fix typo in continents
-      console.log(Object.keys(data[0].currencies)[0]);
-      console.log(data[0].currencies[Object.keys(data[0].currencies)[0]].name); // Fix currency access
-      console.log(
-        Object.values(data[0].languages).toString().split(",").join(",") // Fix typo in join method
-      );
-      // Change "result" to "document.getElementById("result")"
-      document.getElementById("result").innerHTML = `
-        <img src="${data[0].flags.svg}" class="flag-img">
-      `;
-    });
+  
 });

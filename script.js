a5c5bf336193282d7bed34dc5177201ba94885ba
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-17 11:59:14 +0600

    add some js

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 
 searchBtn.addEventListener("click", () => {
-  let countryName = countryInp.value;
+  let countryName = "India";
   let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
   console.log(finalURL);
-  
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      console.log(data[0].capital[0]);
+      console.log(data[0].flags.svg);
+      console.log(data[0].name.common);
+      console.log(data[0].continents[0]);
+    });
 });
